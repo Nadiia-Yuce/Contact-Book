339829c2fc6cfdec5bf870be62f7c4f26b3928c5
Nadiia Yuce <soyka9616@gmail.com>
2024-09-26 23:16:55 +0200

    home page responsive  css

---
+++ src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,17 @@
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+//Налаштування стилів кастомної кнопки (через класи не приміняється)
+export const CustomButton = styled(Button)(({ theme }) => ({
+  backgroundColor: "rgba(114, 142, 168, 0.5)",
+  // color: "white",
+  // "&:hover": {
+  //   backgroundColor: "rgba(114, 142, 168, 0.8)",
+  // },
+  // стилі для екранів шириною 768px і менше
+  [theme.breakpoints.down(768)]: {
+    backgroundColor: "rgba(65, 116, 177, 0.7)", // інший фон для мобільних
+  },
+}));
+
+// Test4@mail.de
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import css from "./HomePage.module.css";
-import Button from "@mui/material/Button";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn, selectUser } from "../../redux/auth/selectors";
+import { CustomButton } from "../../components/CustomButton/CustomButton";
 
 export default function HomePage() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
@@ -13,43 +13,35 @@ export default function HomePage() {
       <div className={css.content}>
         {isLoggedIn ? (
           <>
-            <h1 className={css.title}>{`Hello, ${user.name} !`}</h1>
-            <h3 className={css.subtitle}>Who Will You Call Today? </h3>
+            {/* <h1 className={css.title}>{`Hello, ${user.name} !`}</h1> */}
+            <h1 className={css.loggedTitle}>Who Will You Call Today? </h1>
             <Link to="/contacts">
-              <Button
+              <CustomButton
                 className={css.inviteBtn}
                 variant="contained"
-                sx={{ backgroundColor: "rgba(114, 142, 168, 0.5)" }}
+                // sx={{ backgroundColor: "rgba(114, 142, 168, 0.5)" }}
               >
                 Find Someone
-              </Button>
+              </CustomButton>
             </Link>
           </>
         ) : (
           <>
             <h1 className={css.title}>Hello!</h1>
-            <h3 className={css.subtitle}>
+            <h2 className={css.subtitle}>
               Ready to create or access your contact book?
-            </h3>
+            </h2>
             <div className={css.buttons}>
               <Link to="/login">
-                <Button
-                  className={css.btn}
-                  variant="contained"
-                  sx={{ backgroundColor: "rgba(114, 142, 168, 0.5)" }}
-                >
+                <CustomButton className={css.btn} variant="contained">
                   Log In
-                </Button>
+                </CustomButton>
               </Link>
               <p className={css.subtitle}>or</p>
               <Link to="/registration">
-                <Button
-                  className={css.btn}
-                  variant="contained"
-                  sx={{ backgroundColor: "rgba(114, 142, 168, 0.5)" }}
-                >
+                <CustomButton className={css.btn} variant="contained">
                   Register
-                </Button>
+                </CustomButton>
               </Link>
             </div>
           </>
@@ -58,5 +50,3 @@ export default function HomePage() {
     </div>
   );
 }
-
-/* <h3>Who Will You Call Today? </h3> */
